873c5a6a07cb215c8b292ce5e7d9970a8ee566c6
Haikel Guemar <hguemar@fedoraproject.org>
2018-07-30 11:38:52 +0200

    Move setup to use pbr
    - Simplify setup management
    - Versioning uses OpenStack default postversioning scheme

--- setup.py
+++ setup.py
@@ -3,45 +3,9 @@
 
 """The setup script."""
 
-from setuptools import setup, find_packages
-
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
-requirements = ['GitPython']
-
-setup_requirements = ['pytest-runner']
-
-test_requirements = ['pytest', 'mock']
+from setuptools import setup
 
 setup(
-    author="Jason Joyce",
-    author_email='jjoyce@redhat.com',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-    description="Python wrapper around GitPython",
-    install_requires=requirements,
-    license="MIT license",
-    long_description=readme + '\n\n' + history,
-    include_package_data=True,
-    keywords='git_wrapper',
-    name='git_wrapper',
-    packages=find_packages(include=['git_wrapper']),
-    setup_requires=setup_requirements,
-    test_suite='tests',
-    tests_require=test_requirements,
-    url='https://github.com/release-depot/git_wrapper',
-    version='0.1.0',
-    zip_safe=False,
+    setup_requires=['pbr'],
+    pbr=True
 )
