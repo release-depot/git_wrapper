31777bbb03da53424c2b0eeae2504a237a4f1720
Julie Pichon <jpichon@redhat.com>
2018-08-15 11:51:03 +0100

    Add support for applying a diff
     * Add ability to apply a diff onto a branch (apply_diff)
     * Add ability to reverse a diff that didn't apply (reverse)

--- git_wrapper/commit.py
+++ git_wrapper/commit.py
@@ -17,6 +17,12 @@ logger = logging.getLogger(__name__)
 class GitWrapperCommit(GitWrapperBase):
     """Provides git commit functionality"""
 
+    def _expand_file_path(self, path):
+        full_path = os.path.realpath(os.path.expanduser(path))
+        if not os.path.isfile(full_path):
+            raise exceptions.FileDoesntExistException('%s is not a file.' % full_path)
+        return full_path
+
     def commit(self, message, signoff=False):
         """Create a commit for changes to tracked files in the repo.
            Equivalent to `git commit -a -m <message>`.
@@ -48,9 +54,7 @@ class GitWrapperCommit(GitWrapperBase):
            :param str path: Path to a git-formatted patch file (cf. git format-patch)
         """
         # Expand file (also needed for git-am) and check it exists
-        full_path = os.path.expanduser(path)
-        if not os.path.isfile(full_path):
-            raise exceptions.FileDoesntExistException('%s is not a file.' % full_path)
+        full_path = self._expand_file_path(path)
 
         # Checkout
         try:
@@ -65,6 +69,40 @@ class GitWrapperCommit(GitWrapperBase):
         except git.GitCommandError as ex:
             msg = "Could not apply patch %s on branch %s. Error: %s" % (path, branch_name, ex)
             raise exceptions.ChangeNotAppliedException(msg) from ex
+
+    @reference_exists('branch_name')
+    def apply_diff(self, branch_name, diff_path, message, signoff=False):
+        """Apply a diff on top of the specified branch.
+
+           :param str branch_name: The name of the branch or reference to apply the diff to
+           :param str diff_path: Path to the diff file
+           :param str message: Commit message
+           :param bool signoff: Whether to add signed-off-by to commit message
+        """
+        # Ensure we don't commit more than we mean to
+        if self.repo.is_dirty():
+            msg = "Repository %s contains uncommitted changes. Please clean workspace before proceeding." % self.repo.working_dir
+            raise exceptions.DirtyRepositoryException(msg)
+
+        # Check diff file exists
+        full_path = self._expand_file_path(diff_path)
+
+        # Checkout
+        try:
+            self.repo.git.checkout(branch_name)
+        except git.GitCommandError as ex:
+            msg = "Could not checkout branch %s. Error: %s" % (branch_name, ex)
+            raise exceptions.CheckoutException(msg) from ex
+
+        # Apply the diff
+        try:
+            self.repo.git.apply(full_path)
+        except git.GitCommandError as ex:
+            msg = "Could not apply diff on branch %s. Error: %s" % (branch_name, ex)
+            raise exceptions.ChangeNotAppliedException(msg) from ex
+
+        # Commit
+        self.commit(message, signoff)
 
     @reference_exists('hash_')
     def revert(self, hash_):
@@ -85,3 +123,18 @@ class GitWrapperCommit(GitWrapperBase):
         except git.GitCommandError as ex:
             msg = "Failed to abort git am operation. Error: %s" % ex
             raise exceptions.AbortException(msg) from ex
+
+    def reverse(self, diff_path):
+        """Reverse a diff that was applied to the workspace.
+
+           :param str diff_path: Path to the diff file
+        """
+        full_path = os.path.expanduser(diff_path)
+        if not os.path.isfile(full_path):
+            raise exceptions.FileDoesntExistException('%s is not a file.' % full_path)
+
+        try:
+            self.repo.git.apply(full_path, reverse=True)
+        except git.GitCommandError as ex:
+            msg = "Reversing diff failed. Error: %s" % ex
+            raise exceptions.RevertException(msg) from ex
--- tests/test_commit.py
+++ tests/test_commit.py
@@ -139,6 +139,140 @@ def test_apply_patch_apply_error(mock_repo):
             commit.apply_patch('test_branch', './requirements.txt')
 
 
+def test_apply_diff(mock_repo):
+    """
+    GIVEN GitWrapperCommit initialized with a path and repo
+    WHEN apply_diff is called with a valid branch_name and valid diff_path and valid message
+    THEN index.commit is called
+    """
+    mock_repo.is_dirty.return_value = False
+    commit = GitWrapperCommit('./', mock_repo)
+
+    with patch('git.repo.fun.name_to_object'):
+        commit.apply_diff('test_branch', './requirements.txt', 'message', True)
+    assert commit.repo.git.apply.called is True
+    assert commit.repo.git.commit.called is True
+
+
+def test_apply_diff_on_invalid_branch(mock_repo):
+    """
+    GIVEN GitWrapperCommit initialized with a path and repo
+    WHEN apply_diff is called with an invalid branch_name and valid path
+    THEN ReferenceNotFoundExceptionRaised
+    AND git.apply not called
+    """
+    commit = GitWrapperCommit('./', mock_repo)
+
+    with patch('git.repo.fun.name_to_object') as mock_name_to_object:
+        mock_name_to_object.side_effect = git.exc.BadName()
+        with pytest.raises(exceptions.ReferenceNotFoundException):
+            commit.apply_diff('invalid_branch', './requirements.txt', 'message')
+    assert commit.repo.git.apply.called is False
+
+
+def test_apply_diff_on_dirty_workspace(mock_repo):
+    """
+    GIVEN GitWrapperCommit initialized with a path and repo
+    WHEN apply_diff is called on a dirty repository
+    THEN a DirtyRepositoryException is raised
+    AND git.apply not called
+    """
+    mock_repo.is_dirty.return_value = True
+    commit = GitWrapperCommit('./', mock_repo)
+
+    with patch('git.repo.fun.name_to_object'):
+        with pytest.raises(exceptions.DirtyRepositoryException):
+            commit.apply_diff('test_branch', './requirements.txt', 'message')
+    assert mock_repo.is_dirty.called is True
+    assert commit.repo.git.apply.called is False
+
+
+def test_apply_diff_no_commit_message(mock_repo):
+    """
+    GIVEN GitWrapperCommit initialized with a path and repo
+    WHEN apply_diff is called with valid branch_name, valid diff_path and invalid message
+    THEN CommitMessageMissingException raised
+    AND index.commit not called
+    """
+    mock_repo.is_dirty.return_value = False
+    commit = GitWrapperCommit('./', mock_repo)
+
+    with patch('git.repo.fun.name_to_object'):
+        with pytest.raises(exceptions.CommitMessageMissingException):
+            commit.apply_diff('test_branch', './requirements.txt', '')
+    assert commit.repo.git.commit.called is False
+
+
+def test_apply_diff_not_a_file(mock_repo):
+    """
+    GIVEN GitWrapperCommit initialized with a path and repo
+    WHEN apply_diff is called with valid parameters
+    THEN FileDoesntExistException raised
+    AND git.apply not called
+    """
+    mock_repo.is_dirty.return_value = False
+    commit = GitWrapperCommit('./', mock_repo)
+
+    with patch('git.repo.fun.name_to_object'):
+        with pytest.raises(exceptions.FileDoesntExistException):
+            commit.apply_diff('test_branch', 'doesntexist.txt', 'message')
+    assert commit.repo.git.apply.called is False
+
+
+def test_apply_diff_checkout_error(mock_repo):
+    """
+    GIVEN GitWrapperCommit initialized with a path and repo
+    WHEN apply_diff is called with valid parameters
+    AND checkout fails with an exception
+    THEN a CheckoutException is raised
+    AND index.commit not called
+    """
+    mock_repo.is_dirty.return_value = False
+    mock_repo.git.checkout.side_effect = git.GitCommandError('checkout', '')
+    commit = GitWrapperCommit('./', mock_repo)
+
+    with patch('git.repo.fun.name_to_object'):
+        with pytest.raises(exceptions.CheckoutException):
+            commit.apply_diff('invalid_branch', './requirements.txt', 'my message')
+    assert commit.repo.git.commit.called is False
+
+
+def test_apply_diff_apply_fails(mock_repo):
+    """
+    GIVEN GitWrapperCommit initialized with a path and repo
+    WHEN apply_diff is called with a valid branch_name and valid diff_path and valid message
+    AND git.apply fails with an exception
+    THEN an ChangeNotAppliedException is raised
+    """
+    mock_repo.is_dirty.return_value = False
+    mock_repo.git.apply.side_effect = git.GitCommandError('apply', '')
+    commit = GitWrapperCommit('./', mock_repo)
+
+    with patch('git.repo.fun.name_to_object'):
+        with pytest.raises(exceptions.ChangeNotAppliedException):
+            commit.apply_diff('test_branch', './requirements.txt', 'message')
+    assert commit.repo.git.commit.called is False
+
+
+def test_apply_diff_apply_nothing_to_commit(mock_repo):
+    """
+    GIVEN GitWrapperCommit initialized with a path and repo
+    WHEN apply_diff is called with a valid branch_name and valid diff_path and valid message
+    WHEN commit is called with a valid message
+    AND there are no diff changes
+    THEN git.apply called
+    AND index.commit not called
+    """
+    mock_repo.is_dirty.return_value = False
+    commit = GitWrapperCommit('./', mock_repo)
+    commit.repo.git.diff.return_value = []
+
+    with patch('git.repo.fun.name_to_object'):
+        commit.apply_diff('test_branch', './requirements.txt', 'message')
+    assert commit.repo.git.apply.called is True
+    assert commit.repo.git.commit.called is False
+
+
 def test_revert(mock_repo):
     """
     GIVEN GitWrapperCommit initialized with a path and repo
@@ -206,3 +340,43 @@ def test_abort_error(mock_repo):
 
     with pytest.raises(exceptions.AbortException):
         commit.abort()
+
+
+def test_reverse(mock_repo):
+    """
+    GIVEN GitWrapperCommit initialized with a path and repo
+    WHEN reverse is called with a valid diff_path
+    THEN git.am called
+    """
+    commit = GitWrapperCommit('./', mock_repo)
+
+    commit.reverse('./requirements.txt')
+    assert commit.repo.git.apply.called is True
+
+
+def test_reverse_diff_file_doesnt_exist(mock_repo):
+    """
+    GIVEN GitWrapperCommit initialized with a path and repo
+    WHEN reverse is called with and invalid diff_path
+    THEN FileDoesntExistException raised
+    AND git.apply not called
+    """
+    commit = GitWrapperCommit('./', mock_repo)
+
+    with pytest.raises(exceptions.FileDoesntExistException):
+        commit.reverse('./thisdoesntexist')
+    assert commit.repo.git.apply.called is False
+
+
+def test_reverse_error(mock_repo):
+    """
+    GIVEN GitWrapperCommit initialized with a path and repo
+    WHEN reverse is called with a valid diff_path
+    AND the reverse fails with an exception
+    THEN an RevertException is raised
+    """
+    mock_repo.git.apply.side_effect = git.GitCommandError('apply', '')
+    commit = GitWrapperCommit('./', mock_repo)
+
+    with pytest.raises(exceptions.RevertException):
+        commit.reverse('./requirements.txt')
