5575aac61e7598e68dab5ae1d481e7219ca4d95b
Julie Pichon <jpichon@redhat.com>
2018-08-15 11:48:33 +0100

    Add support for applying a patch
     * Add ability to inject/apply a patch onto a branch (apply_patch)
     * Add ability to abort the git am operation if the patch didn't apply
       cleanly (abort)

--- tests/test_commit.py
+++ tests/test_commit.py
@@ -63,6 +63,82 @@ def test_commit_message_is_not_a_string(mock_repo):
         commit.commit(True)
 
 
+def test_apply_patch(mock_repo):
+    """
+    GIVEN GitWrapperCommit initialized with a path and repo
+    WHEN apply_patch is called with a valid branch_name and valid path
+    THEN git.am is called
+    """
+    commit = GitWrapperCommit('./', mock_repo)
+
+    with patch('git.repo.fun.name_to_object'):
+        commit.apply_patch('test_branch', './requirements.txt')
+    assert commit.repo.git.am.called is True
+
+
+def test_apply_patch_wrong_branch_name(mock_repo):
+    """
+    GIVEN GitWrapperCommit initialized with a path and repo
+    WHEN apply_patch is called with an invalid branch_name and valid path
+    THEN ReferenceNotFoundExceptionRaised
+    AND git.am not called
+    """
+    commit = GitWrapperCommit('./', mock_repo)
+
+    with patch('git.repo.fun.name_to_object') as mock_name_to_object:
+        mock_name_to_object.side_effect = git.exc.BadName()
+        with pytest.raises(exceptions.ReferenceNotFoundException):
+            commit.apply_patch('invalid_branch', './requirements.txt')
+    assert commit.repo.git.am.called is False
+
+
+def test_apply_patch_not_a_file(mock_repo):
+    """
+    GIVEN GitWrapperCommit initialized with a path and repo
+    WHEN apply_patch is called with a valid branch_name and invalid path
+    THEN FileDoesntExistException raised
+    AND git.am not called
+    """
+    commit = GitWrapperCommit('./', mock_repo)
+
+    with patch('git.repo.fun.name_to_object'):
+        with pytest.raises(exceptions.FileDoesntExistException):
+            commit.apply_patch('test_branch', './git_wrapper')
+    assert commit.repo.git.am.called is False
+
+
+def test_apply_patch_checkout_error(mock_repo):
+    """
+    GIVEN GitWrapperCommit initialized with a path and repo
+    WHEN apply_patch is called with a valid branch name and a valid path
+    AND checkout fails with an exception
+    THEN a CheckoutException is raised
+    AND git.am not called
+    """
+    mock_repo.git.checkout.side_effect = git.GitCommandError('checkout', '')
+    commit = GitWrapperCommit('./', mock_repo)
+
+    with patch('git.repo.fun.name_to_object'):
+        with pytest.raises(exceptions.CheckoutException):
+            commit.apply_patch('test_branch', './requirements.txt')
+    assert commit.repo.git.am.called is False
+
+
+def test_apply_patch_apply_error(mock_repo):
+    """
+    GIVEN GitWrapperCommit initialized with a path and repo
+    WHEN apply_patch is called with a valid branch name and a valid path
+    AND git.am fails with an exception
+    THEN a ChangeNotAppliedException is raised
+    """
+    mock_repo.git.am.side_effect = git.GitCommandError('am', '')
+    commit = GitWrapperCommit('./', mock_repo)
+
+    with patch('git.repo.fun.name_to_object'):
+        with pytest.raises(exceptions.ChangeNotAppliedException):
+            commit.apply_patch('test_branch', './requirements.txt')
+
+
 def test_revert(mock_repo):
     """
     GIVEN GitWrapperCommit initialized with a path and repo
@@ -104,3 +180,29 @@ def test_revert_error(mock_repo):
     with patch('git.repo.fun.name_to_object'):
         with pytest.raises(exceptions.RevertException):
             commit.revert('123456')
+
+
+def test_abort(mock_repo):
+    """
+    GIVEN GitWrapperCommit initialized with a path and repo
+    WHEN abort is called
+    THEN git.am called
+    """
+    commit = GitWrapperCommit('./', mock_repo)
+
+    commit.abort()
+    assert commit.repo.git.am.called is True
+
+
+def test_abort_error(mock_repo):
+    """
+    GIVEN GitWrapperCommit initialized with a path and repo
+    WHEN abort is called
+    AND the abort fails with an exception
+    THEN an AbortException is raised
+    """
+    mock_repo.git.am.side_effect = git.GitCommandError('abort', '')
+    commit = GitWrapperCommit('./', mock_repo)
+
+    with pytest.raises(exceptions.AbortException):
+        commit.abort()
