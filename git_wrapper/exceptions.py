44740a6e5dce5a02128bc40e5071624ac41d9336
Julie Pichon <jpichon@redhat.com>
2018-08-15 11:45:33 +0100

    Add support for basic committing and commit revert
     * Add ability to commit the current state of a repo (commit)
     * Add ability to revert a specific commit (revert)
     * Generalise exceptions so it's easier to catch them all

--- git_wrapper/exceptions.py
+++ git_wrapper/exceptions.py
@@ -1,28 +1,43 @@
-class DirtyRepositoryException(Exception):
+class GitWrapperException(Exception):
+    """Superclass for this library's exceptions"""
+    pass
+
+
+class DirtyRepositoryException(GitWrapperException):
     """Repository workspace is dirty."""
     pass
 
 
-class ReferenceNotFoundException(Exception):
+class ReferenceNotFoundException(GitWrapperException):
     """Reference (commit, tag, branch, ...) doesn't exist."""
     pass
 
 
-class CheckoutException(Exception):
+class CheckoutException(GitWrapperException):
     """Error occurred while switching branch."""
     pass
 
 
-class RebaseException(Exception):
+class RebaseException(GitWrapperException):
     """Error occurred during a rebase."""
     pass
 
 
-class AbortException(Exception):
+class AbortException(GitWrapperException):
     """Error occurred while attempting to abort a command."""
     pass
 
 
-class DescribeException(Exception):
+class RevertException(GitWrapperException):
+    """Error occurred while attempting to perform a revert."""
+    pass
+
+
+class DescribeException(GitWrapperException):
     """Error occurred while running the describe command."""
+    pass
+
+
+class CommitMessageMissingException(GitWrapperException):
+    """Cannot create a commit without a commit message."""
     pass
