437aa1cb3d2c6faebf64f77aa9ab1d6cc8249071
Julie Pichon <jpichon@redhat.com>
2018-11-15 15:25:13 +0000

    Redesign: Move clone functionality into base GitRepo
    Also make clone() into a static method, since a GitRepo object won't
    exist or be initialisable until the method is first called.

--- git_wrapper/repo.py
+++ git_wrapper/repo.py
@@ -2,6 +2,8 @@
 """This module acts as an interface for common git tasks"""
 
 import logging
+import os
+import shutil
 
 import git
 from future.utils import raise_from
@@ -64,6 +66,69 @@ class GitRepo(object):
     def git(self):
         """Returns the git command for a given repo"""
         return self.repo.git
+
+    @staticmethod
+    def clone(clone_from, clone_to):
+        """Clone a repository.
+
+           :param str clone_from: The url or path to clone the repo from
+           :param str clone_to: The local path to clone to
+           :return git.Repo: Returns the newly created repo object
+        """
+        clone_to = os.path.realpath(os.path.expanduser(clone_to))
+        logging.debug("Preparing to clone repository %s into directory %s", clone_from, clone_to)
+
+        try:
+            repo = git.repo.base.Repo.clone_from(clone_from, clone_to)
+        except git.GitCommandError as ex:
+            msg = "Error cloning repository {repo}".format(repo=clone_from)
+            raise_from(exceptions.RepoCreationException(msg), ex)
+
+        return GitRepo(repo=repo)
+
+    def destroy_and_reclone(self):
+        """Deletes the current directory and reclone the repository."""
+        # Get local path for the repo
+        local_path = self.repo.working_dir
+
+        self.logger.info("Preparing to delete and reclone repo %s", local_path)
+
+        # Get all of the remotes info
+        remotes = {}
+        for r in self.repo.remotes:
+            remotes[r.name] = r.url
+
+        self.logger.debug("Remotes for %s: %s", local_path, ' '.join(list(remotes)))
+
+        if len(remotes) == 0:
+            msg = "No remotes found for repo {repo}, cannot reclone. Aborting deletion.".format(repo=local_path)
+            raise exceptions.RepoCreationException(msg)
+
+        # Select a remote for the clone, 'origin' by default
+        if "origin" in remotes.keys():
+            default_remote = "origin"
+        else:
+            default_remote = list(remotes)[0]
+
+        self.logger.debug("Default remote for cloning set to '%s'", default_remote)
+
+        # Delete the local repo
+        self.logger.info("Deleting local repo at %s", local_path)
+        shutil.rmtree(local_path, ignore_errors=True)
+
+        # Clone it again
+        repo = self.clone(remotes[default_remote], local_path)
+        self.__setup(repo=repo.repo, path=local_path)
+
+        # Recreate the remotes
+        for name, url in remotes.items():
+            if name != default_remote:
+                try:
+                    self.logger.debug("Adding remote %s", name)
+                    r = self.repo.create_remote(name, url)
+                except git.GitCommandError as ex:
+                    msg = "Issue with recreating remote {remote}. Error: {error}".format(remote=name, error=ex)
+                    raise_from(exceptions.RemoteException(msg), ex)
 
     def describe(self, sha):
         """Return tag and commit info for a given sha
