606d7b46e8898ee1955dba0f6c883d304b9f1c8c
Jason Guiditta <jguiditt@redhat.com>
2021-09-02 14:27:15 -0400

    Change how we generate version for test pypi builds.
    The tooling by default appends '+{hash}' to non-tagged builds,
    which is not compliant with the format required by pypi. This
    patch chops off the '+' bit, so you get a version like '0.2.9.dev5'
    where 'dev5' is the number of commits from the last tag. Provided
    github action puts the env var where the python tooling expects it,
    this should solve the issue for test deploys (production is unchanged).
    
    Signed-off-by: Jason Guiditta <jguiditt@redhat.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,18 @@
 # -*- coding: utf-8 -*-
 
 """The setup script."""
+import os
 
 from setuptools import setup
 
-setup()
+
+def check_if_scm():
+    if os.environ.get('SCM_NO_LOCAL_SCHEME'):
+        return "no-local-version"
+    else:
+        return "node-and-date"
+
+
+setup(
+    use_scm_version={'local_scheme': check_if_scm()}
+)
