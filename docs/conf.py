3264b76e09a1625c9e79a43c62c955b9d09163c5
Jason Guiditta <jguiditt@redhat.com>
2021-10-13 09:09:15 -0400

    Initial automation of changelog generation.
    Signed-off-by: Jason Guiditta <jguiditt@redhat.com>

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,8 @@ author = 'Jason Joyce, Julie Pichon'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode',
+              'sphinx.ext.napoleon', 'myst_parser']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -38,6 +39,11 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = ['.rst', '.md']
+source_suffix = ['.rst', '.md']
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -50,3 +56,20 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- Custom tooling ----------------------------------------------------------
+import os
+import subprocess
+
+def setup(app):
+    app.connect('builder-inited', build_changelog)
+
+def build_changelog(app):
+    docs_dir = os.getcwd()
+    os.chdir('../')
+    root_dir = os.getcwd()
+    cmd = ['bash', f"{root_dir}/tooling/build_changelog", root_dir]
+    change_proc = subprocess.run(cmd, capture_output=True, text=True)
+    print(f"change stderr returns: {change_proc.stderr}")
+    print(f"change stdout returns: {change_proc.stdout}")
+    os.chdir(docs_dir)
